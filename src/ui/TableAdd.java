18e9c1ba210a77c94852b2d4c988784237d4b537
nssb <1065769061@qq.com>
2018-11-02 20:53:49 +0800

    完善监测点、测量点信息在界面上实现增加、修改、删除、显示的功能

--- src/ui/TableAdd.java
+++ src/ui/TableAdd.java
@@ -1,8 +1,8 @@
 package ui;
-
 /*
-   测量点的增加方法
+   监测点的增加方法
  */
+
 import database.Test;
 import org.apache.log4j.Logger;
 
@@ -12,106 +12,108 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 
-public class PointAdd extends JDialog implements ActionListener {
-    private static final Logger LOGGER = Logger.getLogger(PointAdd.class);
-    JButton jb1,jb2;
-    JLabel jl1,jl2,jl3;
-    JTextField jtf1,jtf2,jtf3;
-    JPanel jp1,jp2;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class TableAdd extends JDialog implements ActionListener {
+    private static final Logger LOGGER = Logger.getLogger(TableAdd.class);
+    //定义面板所需各组件
+    JButton jb1, jb2;
+    JLabel jl1, jl2;
+    JTextField jtf1, jtf2 ;
+    JPanel jp1, jp2;
     Myclass owner;
+    Test t1;
+
     //owner代表父窗口
     //title代表窗口名
     //model指定的是模式窗口好事非模式窗口
     static Test dataOper;
-    public  PointAdd(Frame owner,String title,boolean model)
-    {
-        super(owner, title,model);//调用父类构造方法，达到模式对话框效果
-        LOGGER.info("进入添加测量点模式...");
+    public TableAdd(Frame owner, String title, boolean model) {
+        super(owner, title, model);//调用父类构造方法，达到模式对话框效果
+        LOGGER.info("进入添加监测点模式...");
         //定义组件
-        this.owner = (Myclass)owner;
-        jl1=new JLabel("测量点编号");
-        jl2=new JLabel("测量点名称");
-//        jl3=new JLabel("所属监测点编号");
+        this.owner = (Myclass) owner;
+        jl1 = new JLabel("监测点编号");
+        jl2 = new JLabel("监测点名称");
 
-        jtf1=new JTextField(10);
-        jtf2=new JTextField(10);
-//        jtf3=new JTextField(10);
-        jb1=new JButton("新建");
-        jb1.addActionListener(this);
-        jb2=new JButton("取消");
-        jb2.addActionListener(this);
-        jp1=new JPanel(new GridLayout(2,2,20,20));
-        jp2=new JPanel();
 
+        jtf1 = new JTextField(10);
+        jtf2 = new JTextField(10);
+
+        jb1 = new JButton("新建");
+        jb1.addActionListener(this);   //为jb1设置监听事件
+        jb2 = new JButton("取消");
+        jb2.addActionListener(this);   //为jb2设置监听事件
+        jp1 = new JPanel(new GridLayout(3, 2, 10, 10));   //设置JPanel为三行两列的网格布局
+        jp2 = new JPanel();
+        //添加组件
         jp1.add(jl1);
         jp1.add(jtf1);
         jp1.add(jl2);
         jp1.add(jtf2);
-//        jp1.add(jl3);
-//        jp1.add(jtf3);
+
 
 
         jp2.add(jb1);
         jp2.add(jb2);
-        this.add(jp1,BorderLayout.NORTH);
-        this.add(jp2,BorderLayout.SOUTH);
+        this.add(jp1, BorderLayout.NORTH);
+        this.add(jp2, BorderLayout.SOUTH);
         //设置窗体
-        this.setLocation(500,200);
-        this.setSize(300,200);
+        this.setLocation(500, 200);
+        this.setSize(300, 200);
         this.setVisible(true);
         LOGGER.info("显示窗体...");
 //        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
     }
+
+    //按键监听方法
     public void actionPerformed(ActionEvent e) {
+
         if (e.getSource() == jb1) {
-//            String msg=jtf2.getText();   //获得输入的测量点名称
-            String id = jtf1.getText();
-            String name = jtf2.getText();
-            String parameter = jtf3.getText();
-            String monitorName = owner.getTree().getLastSelectedPathComponent().toString();
+            String name =jtf1.getText();
+            LOGGER.debug("获得输入的监测点名称"+name);
+            String position = jtf2.getText();
+
+            DefaultMutableTreeNode treenode = new DefaultMutableTreeNode(name);
             try {
                 dataOper = new Test();
-                dataOper.insertMeasurePointInfo(Integer.parseInt(id), name, Float.parseFloat
-                        (parameter), monitorName);
+                dataOper.insertMonitorInfo(name, position);
             } catch (SQLException e1) {
                 e1.printStackTrace();
-                JOptionPane.showMessageDialog(null, "输入编号已重复，请重新输入编号.", "提示框",
-                        JOptionPane.NO_OPTION);
+                JOptionPane.showMessageDialog(null, "输入编号已重复，请重新输入编号.", "提示框", JOptionPane.NO_OPTION);
                 return;
             } catch (NumberFormatException e2) {
                 e2.printStackTrace();
-                JOptionPane.showMessageDialog(null, "测量点编号或变比输入格式有误, 请重新输入", "提示框", JOptionPane.NO_OPTION);
-
+                JOptionPane.showMessageDialog(null, "监测点编号只能输入数字, 请重新输入", "提示框", JOptionPane.NO_OPTION);
                 return;
             }
-            //获取monitorName的值
+            ((DefaultMutableTreeNode) owner.getTree().getLastSelectedPathComponent()).add(treenode);
+            LOGGER.debug("将新建的测量点名称添加到树模型中");
+            owner.getTree().expandPath(new TreePath(((DefaultMutableTreeNode) this.owner.getTree().getLastSelectedPathComponent()).getPath()));
+            owner.getTree().updateUI();
+            LOGGER.debug("刷新显示树模型");
+            this.setVisible(false);
+            //获取MonitorInfo的信息
             ResultSet result1 = null;
             try {
-                result1 = owner.getDataOper().selectMeasurePointInfo(monitorName);
+                result1 = owner.getDataOper().selectMonitorInfo();
             } catch (SQLException e1) {
 
             }
             owner.generateDataTable(result1);
-            LOGGER.debug("获得输入的测量点名称"+name);
-            DefaultMutableTreeNode treenode = new DefaultMutableTreeNode(name);  //新建树节点存放测 量点名称
 
-                    ((DefaultMutableTreeNode) owner.getTree().getLastSelectedPathComponent()).add
-                    (treenode);//添加该树节点到树模型中
-            owner.getTree().expandPath(new TreePath(((DefaultMutableTreeNode)
-                    this.owner.getTree().getLastSelectedPathComponent()).getPath()));
-            owner.getTree().updateUI();    //刷新以显示该树模型
-            LOGGER.debug("将新建的测量点名称添加到树模型中");
-            this.setVisible(false);
-            LOGGER.debug("刷新显示树模型");
         } else if (e.getSource() == jb2) {
             this.setVisible(false);
         }
     }
+
+
 
 
 //    public void updateInspector(String name, String  ){
@@ -122,10 +124,10 @@ public class PointAdd extends JDialog implements ActionListener {
 //        }
 //    }
 
-    public static void main(String[] args)
-    {
-
-    }
+//    public static void main(String[] args)
+//    {
+//
+//    }
 
 
 }
